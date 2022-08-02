7780b4941f800852907fa8fbcd5c7f40c7ef1f51
kkauma <kylekauma@gmail.com>
2022-08-02 18:43:17 -0500

    add logic for unshift, run tests

--- Singly_Linked_List/app.js
+++ Singly_Linked_List/app.js
@@ -50,6 +50,18 @@ class SinglyLinkedList {
     }
     return oldHead;
   }
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 const list = new SinglyLinkedList();
