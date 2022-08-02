63dc4e9da65a2039fa2d84692779a2ba6650e4d3
kkauma <kylekauma@gmail.com>
2022-08-02 18:03:53 -0500

    add boilerplate code for singly linked list

---
+++ Singly_Linked_List/app.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
