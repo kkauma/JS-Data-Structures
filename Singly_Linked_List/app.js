cf60d41b25c414692d568fa69b3e88192934f638
kkauma <kylekauma@gmail.com>
2022-08-02 18:09:26 -0500

    create push method for new node

--- Singly_Linked_List/app.js
+++ Singly_Linked_List/app.js
@@ -11,4 +11,15 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    const node = new Node(val);
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+  }
 }
