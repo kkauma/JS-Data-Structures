0b958a77ccd92337a55ad998bb4a5c18bb84fe8e
kkauma <kylekauma@gmail.com>
2022-08-02 18:34:57 -0500

    add logic for pop method

--- Singly_Linked_List/app.js
+++ Singly_Linked_List/app.js
@@ -23,11 +23,35 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
 
 const list = new SinglyLinkedList();
+// console.log(list);
+list.push("hello");
+// console.log(list);
+list.push("world");
+list.push("vercel");
 console.log(list);
-list.push("hi");
+list.pop();
 console.log(list);
-list.push("you");
+list.pop();
+list.pop();
+list.pop();
 console.log(list);
