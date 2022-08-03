38795b8965928e7146cc6bde8aa130654cb264a7
kkauma <kylekauma@gmail.com>
2022-08-03 11:31:01 -0500

    write logic for pushing a node to the list

--- Doubly_Linked_List/app.js
+++ Doubly_Linked_List/app.js
@@ -12,4 +12,17 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
