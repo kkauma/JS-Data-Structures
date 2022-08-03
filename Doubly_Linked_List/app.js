fec36a6938d77f21ca461dd3e4a3f89f31afef17
kkauma <kylekauma@gmail.com>
2022-08-03 11:56:59 -0500

    write logic for adding a node to the beginning of the list

--- Doubly_Linked_List/app.js
+++ Doubly_Linked_List/app.js
@@ -53,6 +53,19 @@ class DoublyLinkedList {
     this.length--;
     return oldHead;
   }
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 list = new DoublyLinkedList();
