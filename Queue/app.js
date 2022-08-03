430bb5de60378ac5a5a9356767f443182e865333
kkauma <kylekauma@gmail.com>
2022-08-03 18:11:16 -0500

    implement a queue using an array

---
+++ Queue/app.js
@@ -0,0 +1,30 @@
+// Queue
+// FIFO
+// Insertion: O(1)
+// Removal: O(1)
+
+// Queue using an array
+const q = [];
+q.push("FIRST");
+q.push("SECOND");
+q.push("THIRD");
+console.log(q.shift());
+console.log(q.shift());
+console.log(q.shift());
+console.log(q.shift());
+
+// Queue (singly linked list)
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+// class Queue {
+//   constructor() {
+//     this.first = null;
+//     this.last = null;
+//     this.size = 0;
+//   }
+// }
