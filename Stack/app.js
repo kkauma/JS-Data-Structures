0de46b95bf94eb112c6d7480fc6132f2a492fc72
kkauma <kylekauma@gmail.com>
2022-08-03 17:43:45 -0500

    add code for building a stack using an array

---
+++ Stack/app.js
@@ -0,0 +1,14 @@
+// Stacks
+// LIFO
+
+// Stack with an array
+const stack = [];
+stack.push("google");
+stack.push("stripe");
+stack.push("youtube");
+console.log(stack);
+console.log(stack.pop());
+stack.push("amazon");
+console.log(stack);
+console.log(stack.pop());
+console.log(stack);
