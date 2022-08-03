b3f0e97f29d007bd47a7c7a071a56cb14435966f
kkauma <kylekauma@gmail.com>
2022-08-03 12:17:28 -0500

    refactor code

--- Doubly_Linked_List/app.js
+++ Doubly_Linked_List/app.js
@@ -96,12 +96,13 @@ class DoublyLinkedList {
   }
   insert(idx, val) {
     if (idx < 0 || idx > this.length) return false;
-    if (idx === 0) return this.unshift(val);
-    if (idx === this.length) return this.push(val);
+    if (idx === 0) return !!this.unshift(val);
+    if (idx === this.length) return !!this.push(val);
 
     const newNode = new Node(val);
     let beforeNode = this.get(idx - 1);
     let afterNode = beforeNode.next;
+
     beforeNode.next = newNode;
     newNode.prev = beforeNode;
     newNode.next = afterNode;
