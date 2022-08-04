5fb862afa1b0fddbb0b3aa33bb4ed4e4ed6a23a2
kkauma <kylekauma@gmail.com>
2022-08-03 20:27:41 -0500

    add boilerplate code for BST

---
+++ Trees/BST/app.js
@@ -0,0 +1,22 @@
+// Binary Search Trees
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
+
+let tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
+console.log(tree);
