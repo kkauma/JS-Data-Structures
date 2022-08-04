633521a263343b82217e64ce9203fc08ed81af30
kkauma <kylekauma@gmail.com>
2022-08-03 20:40:22 -0500

    write logic for inserting a node into a binary search tree

--- Trees/BST/app.js
+++ Trees/BST/app.js
@@ -1,4 +1,4 @@
-// Binary Search Trees
+// Binary Search Tree
 
 class Node {
   constructor(val) {
@@ -12,11 +12,34 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+  insert(val) {
+    const newNode = new Node(val);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (val === current.val) return undefined;
+        if (val < current.val) {
+          if (current.left === null) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        } else if (val > current.val) {
+          if (current.right === null) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+  }
 }
 
 let tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
 console.log(tree);
